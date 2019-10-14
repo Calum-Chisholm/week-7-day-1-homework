fb24189af65ce0df3bdd9d0185e125b7bfcc7d0e
Calum Chisholm <calum.chisholm@hotmail.com>
2019-10-14 16:57:46 +0100

    homework mvp done

--- src/app.js
+++ src/app.js
@@ -4,10 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-
+      toDos:
+      [{name: 'Restock fridge', isDone: false},
+      {name: 'Walk the dog', isDone: true},
+      {name: 'Iron Red Shirt', isDone: false}],
+      newTask: ""
     },
     methods: {
-
-      }
-  });
+      completedTask: function(task) {
+        task.isDone = true;
+      },
+      saveNewTask: function() {
+      this.toDos.push({name: this.newTask, isDone: false});
+      this.newTask = "";
+    }
+   }
+ });
 });
